dc0dc2b16c44ba98fc0e0a882e73e2bcbaae6ad5
NirmalyaRakshit <64269311+NirmalyaRakshit@users.noreply.github.com>
2023-11-18 19:36:25 +0000

    Add files via upload

--- coordReader.c
+++ coordReader.c
@@ -4,82 +4,88 @@
 #include<string.h>
 #include<stdbool.h>
 
-// ... (The rest of the header comments remain unchanged.)
+/*This code is for reading and writing to files for the 2023-24 COMP528 CA1*/
+
+/*Use the functions in this file to read from the input file, and write to the output file*/
+
+/*You should use this file when compiling your code*/
+
+/*Declare these functions at the top of each 'main' file*/
+
+/*If there are any issues with this code, please contact: h.j.forbes@liverpool.ac.uk*/
 
 int readNumOfCoords(char *fileName);
 double **readCoords(char *filename, int numOfCoords);
-void writeTourToFile(int *tour, int tourLength, char *filename);
 
+/*Gets the number of the coordinates in the file. Returns as a single integer*/
 int readNumOfCoords(char *filename){
-    FILE *file = fopen(filename, "r");
-    int numOfCoords = 0;
+	FILE *file = fopen(filename, "r");
+	int numOfCoords = 0;
 
-    if(file == NULL){
-        return -1; // Indicate file opening error.
-    }
+	if(file == NULL){
+		return -1;
+	}
 
-    char line[100];
-    while(fgets(line, sizeof(line), file) != NULL){
-        numOfCoords++;
-    }
+	char line[100];
 
-    fclose(file); // Close the file after reading the number of coordinates.
+	while(fgets(line, sizeof(line), file) != NULL){
+		numOfCoords++;
+	}
+	
     return numOfCoords;
 }
 
+/*Gets the data from the file. Returns as an array of doubles, Ignores the first integer*/
 double **readCoords(char *filename, int numOfCoords){
-    FILE *file = fopen(filename,"r");
-    char line[100];
+	FILE *file = fopen(filename,"r");
+    int i;
 
+	char line[100];
+    
     if(file == NULL) {
         printf("Unable to open file: %s", filename);
         return NULL;
     }
 
-    double **coords = (double **)malloc(numOfCoords * sizeof(double *));
-    if (coords == NULL) {
-        perror("Memory Allocation Failed for Coordinates Array");
-        return NULL;
-    }
+	double **coords = (double **)malloc(numOfCoords * sizeof(double *));
 
-    for(int i = 0; i < numOfCoords; i++){
-        coords[i] = (double *)malloc(2 * sizeof(double));
-        if (coords[i] == NULL){
-            perror("Memory Allocation Failed for Coordinate Pair");
-            // Properly free previously allocated memory
-            for(int j = 0; j < i; j++) {
-                free(coords[j]);
-            }
-            free(coords);
-            return NULL;
-        }
-    }
+	for(i = 0; i < numOfCoords; i++){
+		coords[i] = (double *) malloc(2 * sizeof(int));
+		if (coords[i] == NULL){
+			perror("Memory Allocation Failed");
+		}
+	}
 
-    int lineNum = 0;
-    while(fgets(line, sizeof(line), file) != NULL){
-        double x, y;
-        if (sscanf(line, "%lf,%lf", &x, &y) == 2){
-            coords[lineNum][0] = x;
-            coords[lineNum][1] = y;
-            lineNum++;
-        }
-    }
+	int lineNum = 0;
+	while(fgets(line, sizeof(line), file) != NULL){
+		double x, y;
+		if (sscanf(line, "%lf,%lf", &x, &y) == 2){
+			coords[lineNum][0] = x;
+			coords[lineNum][1] = y;
+			lineNum++;
+		}
+	}
 
-    fclose(file); // Close the file after reading the coordinates.
-    return coords;
+	return coords;
 }
 
-void writeTourToFile(int *tour, int tourLength, char *filename){
-    FILE *file = fopen(filename, "w");
-    if(file == NULL){
-        printf("Unable to open file: %s", filename);
-        return; // Exit the function if file opening fails.
-    }
+void *writeTourToFile(int *tour, int tourLength, char *filename){
+	
+	FILE *file = fopen(filename, "w");
+	int i;	
+	
+	if(file == NULL){
+		printf("Unable to open file: %s", filename);
+		return NULL;
+	}
 
-    fprintf(file, "%d\n", tourLength); // Write the tour length.
-    for(int i = 0; i < tourLength; i++) {
+	fprintf(file, "%d \n", tourLength);
+
+	printf("Writing output data\n");
+    for(i=0; i < tourLength; i++) {
         fprintf(file, "%d ", tour[i]);
     }
 
-    fclose(file); // Close the file after writing the tour.
+	
+
 }
